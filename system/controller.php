457a5d93928b582525ce040a03cbfc0195014712
Rougin Gutib <rougingutib@gmail.com>
2014-02-18 20:53:38 +0800

    remove <<<<<HEAD. such noob

--- system/controller.php
+++ system/controller.php
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
 <?php if ( ! defined('SYSTEM')) exit('No direct script access allowed');
-class Controller
+abstract class Controller
 {
 	public function __construct()
 	{
 		require_once SYSTEM . 'load.php';
 	}
 }
-=======
-<?php
-abstract class Controller
-{
-	// protected $registry;
-	// function __construct($registry) { $this->registry = $registry; }
-	// abstract function index();
-}
->>>>>>> 521c8223a219db5c97689ed9578dfa8794b6c930
--- system/model.php
+++ system/model.php
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 <?php if ( ! defined('SYSTEM')) exit('No direct script access allowed');
-class Model
+abstract class Model
 {
 	
 }
-=======
-<?php
-abstract class Model
-{
-	// protected $registry;
-	// function __construct($registry) { $this->registry = $registry; }
-	// abstract function index();
-}
->>>>>>> 521c8223a219db5c97689ed9578dfa8794b6c930
