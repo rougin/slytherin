017c444dbc57e7192c645d34960c73e391f8ee52
Rougin Gutib <rougin.royce@gmail.com>
2014-02-19 23:46:48 +0800

    added libraries and helpers

---
+++ application/libraries/input.php
@@ -0,0 +1,22 @@
+<?php
+class Input
+{
+	public function post($name = NULL)
+	{
+		/* if the selected $_POST value does not exists, set a NULL value */
+		if (!isset($_POST[$name]) && $name != NULL)
+		{
+			return NULL;
+		}
+		/* else return the selected $_POST value */
+		if ($name != NULL)
+		{
+			return $_POST[$name];
+		}
+		/* else return the whole $_POST value */
+		else
+		{
+			return $_POST;
+		}
+	}
+}
