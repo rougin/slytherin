8246b4d182665ab548c5d15475cfbf41456f4272
Rougin Gutib <rougin.royce@gmail.com>
2014-02-27 20:02:46 +0800

    added autoload feature

--- system/load.php
+++ system/load.php
@@ -3,11 +3,19 @@ class Load
 {
 	public function file($name, $file)
 	{
-		require(APPLICATION . $name . '/' . strtolower($file) . '.php');
+		require APPLICATION . $name . '/' . strtolower($file) . '.php';
 		if ($name != 'helpers')
 		{
 			return new $file();
 		}
+	}
+
+	public function files($name, $files)
+	{
+		foreach ($files as $file)
+		{
+			self::file($name, $file);
+		}	
 	}
 
 	public function model($file)
@@ -21,17 +29,27 @@ class Load
 		return self::file('libraries', $file);
 	}
 
+	public function libraries($files)
+	{
+		return self::files('libraries', $files);
+	}
+
 	public function helper($file)
 	{
 		return self::file('helpers', $file);
 	}
 
-    public function view($file, $data = NULL)
+	public function helpers($files)
 	{
-	    if ($data != NULL)
-	    {
-	        extract($data);
-	    }
-		require_once APPLICATION . 'views/' . $file;
+		return self::files('helpers', $files);
+	}
+
+	public function view($file, $data = NULL)
+	{
+		if ($data != NULL)
+		{
+			extract($data);
+		}
+		require_once APPLICATION . 'views/' . $file . '.php';
 	}
 }
