16a315d9326c7663e9b0bd8f1ba2ed416b1bd98f
Rougin Gutib <rougin.royce@gmail.com>
2014-02-27 20:02:57 +0800

    updated the documentation

--- application/controllers/documentation.php
+++ application/controllers/documentation.php
@@ -1,4 +1,4 @@
-<?php if ( ! defined('SYSTEM')) exit('No direct script access allowed');
+<?php if (!defined('SYSTEM')) exit('No direct script access allowed');
 class Documentation extends Controller
 {
 
