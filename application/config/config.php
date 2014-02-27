db8036a12d99f9dad3ec644cdfbcbef808ae5f34
Rougin Gutib <rougin.royce@gmail.com>
2014-02-27 17:49:23 +0800

    added small documentation on config.php

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,23 @@
 <?php
+/*
+This is the configuration file of Slytherin. Here you can
+add configurations on the system as well as configurations on
+your libraries that comes from Composer.
+*/
+
+/*
+ROUTES
+You can set your own routing rules here. Routes can either be
+specified using wildcards(soon) or Regular Expressions(soon)
+*/
 $routes['default_controller'] = 'helloworld';
+
+/*
+DATABASE CONFIGURATION
+Here is the details for configuring your database. Just enter
+the following information required and it will be used everytime
+the system wants to connect to the database.
+*/
 $database['type'] = 'mysql';
 $database['hostname'] = 'localhost';
 $database['username'] = 'root';
