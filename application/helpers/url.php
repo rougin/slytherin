40810234b198c8dc8c3e74433c4d7ad53fe73697
Rougin Gutib <rougin.royce@gmail.com>
2014-03-02 21:24:30 +0800

    added URL::segment() method

--- application/helpers/url.php
+++ application/helpers/url.php
@@ -5,4 +5,11 @@ class URL
 	{
 		return 'http://' . $_SERVER['HTTP_HOST'] . '/' . $url;
 	}
+
+	public function segment($number)
+	{
+		$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+		$segments = explode('/', $path);
+		return $segments[$number];
+	}
 }
