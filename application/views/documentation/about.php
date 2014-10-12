79fae0c5947d31b1204d908586e3863ec038bd1c
Rougin Gutib <rougingutib@gmail.com>
2014-10-12 23:03:47 +0800

    Deleted documentation and other files

--- application/views/documentation/about.php
+++
@@ -1,7 +0,0 @@
-<h3 class="page-header">What is Slytherin?</h3>
-<div class="row-placeholder">
-Slytherin is a simple and easy to understand PHP skeleton framework that uses a Model-View-Controller (MVC) software pattern. Unlike other other frameworks
-that provides big libraries that is unnecessary in building your wep application or site, Slytherin uses Composer to add, update or even remove external
-libraries with ease. With these, you have the right to select the libraries that you want to use, minimizing the size of your project and it also provides
-flexibility to the framework.
-</div>
--- application/views/welcome.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>Slytherin PHP</title>
-</head>
-<body>
-<p>Hello world.. Hello Slytherin.</p>
-</body>
-</html>
