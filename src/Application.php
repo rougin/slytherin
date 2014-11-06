afd9bb1317b1004a75c4e7fb6293da59316d8b70
Rougin Gutib <rougingutib@gmail.com>
2014-11-06 17:29:01 +0800

    Added Pux as the main router, added routes.php, improved accio, and cleaned up some mess

--- src/Application.php
+++ src/Application.php
@@ -1,5 +1,8 @@
 <?php namespace Slytherin;
 
+use Pux\Executor;
+use Pux\Mux;
+
 /**
  * The Slytherin Application
  */
@@ -7,17 +10,24 @@
 class Application
 {
 
-	protected $_router = NULL;
+	protected $_constructorArguments = array();
+
+	protected $_controller = NULL;
+	protected $_methods    = array();
+	protected $_router     = NULL;
 
 	/**
 	 * Load the necessary configurations
 	 */
-	public function __construct(Router $router)
+	public function __construct(Mux $router)
 	{
+		$this->_checkDirectories();
+		$this->_defineUrls();
+		
 		$this->_router = $router;
 
 		/**
-		 * Autoload controller files
+		 * Autoload files
 		 */
 
 		spl_autoload_register(function ($class) {
@@ -37,20 +47,78 @@ class Application
 
 	/**
 	 * Run the application
-	 * 
-	 * @return object
 	 */
 	public function run()
 	{
-		$this->_checkDirectories();
-		$this->_checkMethod();
-		$controller = $this->_loadController();
+		$this->_getController();
+
+		foreach ($this->_methods as $method => $parameters) {
+			if ($method == '__construct') continue;
+
+			$options  = (empty($this->_constructorArguments)) ? array() : array('constructor_args' => $this->_constructorArguments);
+			
+			$regex    = array();
+			$segments = NULL;
+
+			/**
+			 * Implode the parameters and create a regex pattern
+			 */
+			
+			if (is_array($parameters)) {
+				foreach ($parameters as $parameter => $defaultValue) {
+					$segments .= '/:' . $parameter;
+
+					$regex[$parameter] = '\w+';
+					$regex[$parameter] = (gettype($defaultValue) == 'integer') ? '\d+' : $regex[$parameter];
+				}
+
+				/**
+				 * Get the options
+				 */
+
+				$options['default'] = $parameters;
+				$options['require'] = $regex;
+			}
+
+			/**
+			 * Set the HTTP verb for the specified method
+			 */
+
+			$httpMethod = ($method == 'destroy' || $method == 'delete') ? 'delete' : 'get';
+			$httpMethod = ($method == 'index') ? 'get' : $httpMethod;
+			$httpMethod = ($method == 'store') ? 'post' : $httpMethod;
+			$httpMethod = ($method == 'update') ? 'put' : $httpMethod;
+
+			/**
+			 * Add an additional pattern for 'create' and 'edit' methods
+			 */
+
+			$pattern = '/' . $this->_controller . $segments;
+			$pattern = ($method == 'create') ? $pattern . '/create' : (($method == 'edit') ? $pattern . '/edit' : $pattern);
+
+			/**
+			 * Define the specified route
+			 */
+
+			$this->_router->$httpMethod($pattern, array(ucfirst($this->_controller), $method), $options);
+		}
 
 		/**
-		 * Run the specified parameters
+		 * Set the URL to be dispatch
 		 */
+
+		$url = str_replace(BASE_URL, '', CURRENT_URL);
+		$url = (substr($url, -1) == '/') ? substr($url, 0, strlen($url) - 1) : $url;
+
+		$this->_router->any('/', array('Welcome', 'index'));
 		
-		echo call_user_method_array($this->_router->getMethod(), $controller, array_splice($this->_router->getParameters(), 0));
+		/**
+		 * Dispatch and execute the route
+		 */
+
+		$route = $this->_router->dispatch('/' . $url);
+
+		echo Executor::execute($route);
 	}
 
 	/**
@@ -63,6 +131,7 @@ class Application
 		$directories = array(
 			'app/',
 			'app/controllers/',
+			'app/config/',
 			'app/libraries/',
 			'app/models/',
 			'app/views/'
@@ -76,48 +145,61 @@ class Application
 	}
 
 	/**
-	 * Check if the method really exists from the specified controller
-	 * 
-	 * @return error
+	 * Define the base and current urls
 	 */
-	protected function _checkMethod()
+	protected function _defineUrls()
 	{
-		if ( ! method_exists($this->_router->getController(), $this->_router->getMethod())) {
-			return trigger_error('"' . $this->_router->getMethod() . '" method cannot be found from the "' . $this->_router->getController() . '" controller', E_USER_ERROR);
+		/**
+		 * Get the base url from the $_SERVER['HTTP_HOST']
+		 */
+
+		if (isset($_SERVER['HTTP_HOST'])) {
+			$baseUrl = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST']
+				. substr($_SERVER['SCRIPT_NAME'], 0, -strlen(basename($_SERVER['SCRIPT_NAME'])));
+		} else {
+			$baseUrl = 'http://localhost/';
 		}
+
+		/**
+		 * Check the HTTP method from the form
+		 */
+
+		if (isset($_POST['_method']) && ($_POST['_method'] == 'PUT' || $_POST['_method'] == 'DELETE')) {
+			$_SERVER['REQUEST_METHOD'] = $_POST['_method'];
+			unset($_POST['_method']);
+		}
+
+		/**
+		 * Define the following URLs
+		 */
+
+		define('BASE_URL', $baseUrl);
+		define('CURRENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 	}
 
 	/**
-	 * Load the specified controller
-	 * 
-	 * @return error | object
+	 * Check the controller and get its contents
 	 */
-	protected function _loadController()
+	protected function _getController()
 	{
-		$controllerFile        = 'app/controllers/' . $this->_router->getController() . '.php';
-		$constructorParameters = array();
-
-		if ( ! file_exists($controllerFile)) {
-			return trigger_error('"' . $this->_router->getController() . '" controller cannot be found from the "app/controllers/" directory', E_USER_ERROR);
-		}
-
 		/**
-		 * Create a new Reflection Class and get its constructor
+		 * Seperate the links from the string difference of BASE_URL and CURRENT_URL
 		 */
 
-		$class = new \ReflectionClass($this->_router->getController());
+		$segments = explode('/', str_replace(BASE_URL, '', CURRENT_URL));
 
 		/**
-		 * Check if the controller is extended to the base controller
+		 * Set the first index as the controller
 		 */
+		
+		if (empty($segments[0])) return 0;
 
-		if ( ! $class->getParentClass() || ($class->getParentClass()->name != 'Controller' && $class->getParentClass()->name != 'Slytherin\Controller')) {
-			return trigger_error('Class "' . $this->_router->getController() . '" must be extended to a "Controller" class', E_USER_ERROR);
-		}
+		$this->_controller = $segments[0];
 
+		$class       = new \ReflectionClass(ucfirst($this->_controller));
 		$constructor = $class->getConstructor();
 
-		if ($constructor) {
+		if ($constructor && count($constructor->getParameters()) != 0) {
 			foreach ($constructor->getParameters() as $parameter) {
 				/**
 				 * Get the class name without needing the class to be loaded
@@ -125,14 +207,27 @@ class Application
 				
 				preg_match('/\[\s\<\w+?>\s([\w]+)/s', $parameter->__toString(), $matches);
 				$object = isset($matches[1]) ? $matches[1] : NULL;
-
 				if ($object) {
-					$constructorParameters[] = new $object();
+					$this->_constructorArguments[] = new $object();
 				}
 			}
 		}
 
-		return $class->newInstanceArgs($constructorParameters);
+		foreach ($class->getMethods() as $method) {
+			/**
+			 * Add the curent method to the list of methods
+			 */
+
+			$this->_methods[$method->name] = NULL;
+
+			/**
+			 * Get the parameters for the each specified method
+			 */
+
+			foreach ($method->getParameters() as $parameter) {
+				$this->_methods[$method->name][$parameter->name] = ($parameter->isOptional()) ? $parameter->getDefaultValue() : NULL;
+			}
+		}
 	}
 
 }
