dd7e46bd85a97756b2b8e0bac4c43a03a50756a4
Rougin Royce Gutib <rougingutib@gmail.com>
2016-09-21 20:54:10 +0800

    Update HTTP fixture

--- tests/Fixture/Http/Message.php
+++ tests/Fixture/Http/Message.php
@@ -21,75 +21,247 @@ use Psr\Http\Message\StreamInterface;
  */
 class Message implements MessageInterface
 {
+    /**
+     * @var string
+     */
     private $version = '1.1';
-    private $headers = array();
+
+    /**
+     * @var array
+     */
+    private $headers = [];
+
+    /**
+     * @var \Psr\Http\Message\StreamInterface
+     */
     private $body;
 
-    public function __construct($version = '1.1', array $headers = array(), StreamInterface $body = null)
+    /**
+     * @param string                                 $version
+     * @param array                                  $headers
+     * @param \Psr\Http\Message\StreamInterface|null $body
+     */
+    public function __construct($version = '1.1', array $headers = [], StreamInterface $body = null)
     {
         $this->version = $version;
         $this->headers = $headers;
-        $this->body = null === $body ? new Stream() : $body;
+
+        $this->body = (null === $body) ? new Stream : $body;
     }
 
+    /**
+     * Retrieves the HTTP protocol version as a string.
+     *
+     * The string MUST contain only the HTTP version number (e.g., "1.1", "1.0").
+     *
+     * @return string HTTP protocol version.
+     */
     public function getProtocolVersion()
     {
         return $this->version;
     }
 
+    /**
+     * Return an instance with the specified HTTP protocol version.
+     *
+     * The version string MUST contain only the HTTP version number (e.g.,
+     * "1.1", "1.0").
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * new protocol version.
+     *
+     * @param string $version HTTP protocol version
+     * @return static
+     */
     public function withProtocolVersion($version)
     {
-        throw new \BadMethodCallException('Not implemented.');
+        $this->version = $version;
+
+        return clone $this;
     }
 
+    /**
+     * Retrieves all message header values.
+     *
+     * The keys represent the header name as it will be sent over the wire, and
+     * each value is an array of strings associated with the header.
+     *
+     *     // Represent the headers as a string
+     *     foreach ($message->getHeaders() as $name => $values) {
+     *         echo $name . ": " . implode(", ", $values);
+     *     }
+     *
+     *     // Emit headers iteratively:
+     *     foreach ($message->getHeaders() as $name => $values) {
+     *         foreach ($values as $value) {
+     *             header(sprintf('%s: %s', $name, $value), false);
+     *         }
+     *     }
+     *
+     * While header names are not case-sensitive, getHeaders() will preserve the
+     * exact case in which headers were originally specified.
+     *
+     * @return string[][] Returns an associative array of the message's headers. Each
+     *     key MUST be a header name, and each value MUST be an array of strings
+     *     for that header.
+     */
     public function getHeaders()
     {
         return $this->headers;
     }
 
+    /**
+     * Retrieves a message header value by the given case-insensitive name.
+     *
+     * This method returns an array of all the header values of the given
+     * case-insensitive header name.
+     *
+     * If the header does not appear in the message, this method MUST return an
+     * empty array.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @return string[] An array of string values as provided for the given
+     *    header. If the header does not appear in the message, this method MUST
+     *    return an empty array.
+     */
     public function hasHeader($name)
     {
         return isset($this->headers[$name]);
     }
 
+    /**
+     * Retrieves a message header value by the given case-insensitive name.
+     *
+     * This method returns an array of all the header values of the given
+     * case-insensitive header name.
+     *
+     * If the header does not appear in the message, this method MUST return an
+     * empty array.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @return string[] An array of string values as provided for the given
+     *    header. If the header does not appear in the message, this method MUST
+     *    return an empty array.
+     */
     public function getHeader($name)
     {
-        return $this->hasHeader($name) ? $this->headers[$name] : array();
+        return $this->hasHeader($name) ? $this->headers[$name] : [];
     }
 
+    /**
+     * Retrieves a comma-separated string of the values for a single header.
+     *
+     * This method returns all of the header values of the given
+     * case-insensitive header name as a string concatenated together using
+     * a comma.
+     *
+     * NOTE: Not all header values may be appropriately represented using
+     * comma concatenation. For such headers, use getHeader() instead
+     * and supply your own delimiter when concatenating.
+     *
+     * If the header does not appear in the message, this method MUST return
+     * an empty string.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @return string A string of values as provided for the given header
+     *    concatenated together using a comma. If the header does not appear in
+     *    the message, this method MUST return an empty string.
+     */
     public function getHeaderLine($name)
     {
         return $this->hasHeader($name) ? implode(',', $this->headers[$name]) : '';
     }
 
+    /**
+     * Return an instance with the provided value replacing the specified header.
+     *
+     * While header names are case-insensitive, the casing of the header will
+     * be preserved by this function, and returned from getHeaders().
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * new and/or updated header and value.
+     *
+     * @param string $name Case-insensitive header field name.
+     * @param string|string[] $value Header value(s).
+     * @return static
+     * @throws \InvalidArgumentException for invalid header names or values.
+     */
     public function withHeader($name, $value)
     {
         $this->headers[$name] = [ $value ];
 
-        return $this;
+        return clone $this;
     }
 
+    /**
+     * Return an instance with the specified header appended with the given value.
+     *
+     * Existing values for the specified header will be maintained. The new
+     * value(s) will be appended to the existing list. If the header did not
+     * exist previously, it will be added.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * new header and/or value.
+     *
+     * @param string $name Case-insensitive header field name to add.
+     * @param string|string[] $value Header value(s).
+     * @return static
+     * @throws \InvalidArgumentException for invalid header names or values.
+     */
     public function withAddedHeader($name, $value)
     {
         $this->headers[$name][] = $value;
 
-        return $this;
+        return clone $this;
     }
 
+    /**
+     * Return an instance without the specified header.
+     *
+     * Header resolution MUST be done without case-sensitivity.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that removes
+     * the named header.
+     *
+     * @param string $name Case-insensitive header field name to remove.
+     * @return static
+     */
     public function withoutHeader($name)
     {
         throw new \BadMethodCallException('Not implemented.');
     }
 
+    /**
+     * Gets the body of the message.
+     *
+     * @return StreamInterface Returns the body as a stream.
+     */
     public function getBody()
     {
         return $this->body;
     }
 
+    /**
+     * Return an instance with the specified message body.
+     *
+     * The body MUST be a StreamInterface object.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return a new instance that has the
+     * new body stream.
+     *
+     * @param StreamInterface $body Body.
+     * @return static
+     * @throws \InvalidArgumentException When the body is not valid.
+     */
     public function withBody(StreamInterface $body)
     {
         $this->body = $body;
 
-        return $this;
+        return clone $this;
     }
 }
--- tests/Fixture/Http/ServerRequest.php
+++ tests/Fixture/Http/ServerRequest.php
@@ -16,21 +16,72 @@ use Psr\Http\Message\StreamInterface;
 use Psr\Http\Message\UriInterface;
 
 /**
+ * Server Request.
+ *
  * @author Kévin Dunglas <dunglas@gmail.com>
  */
 class ServerRequest extends Message implements ServerRequestInterface
 {
+    /**
+     * @var string
+     */
     private $requestTarget;
+
+    /**
+     * @var string
+     */
     private $method;
+
+    /**
+     * @var \Psr\Http\Message\UriInterface
+     */
     private $uri;
+
+    /**
+     * @var array
+     */
     private $server;
+
+    /**
+     * @var array
+     */
     private $cookies;
+
+    /**
+     * @var array
+     */
     private $query;
+
+    /**
+     * @var array
+     */
     private $uploadedFiles;
+
+    /**
+     * @var array|null
+     */
     private $data;
+
+    /**
+     * @var array
+     */
     private $attributes;
 
-    public function __construct($version = '1.1', array $headers = array(), StreamInterface $body = null, $requestTarget = '/', $method = 'GET', UriInterface $uri = null, array $server = array(), array $cookies = array(), array $query = array(), array $uploadedFiles = array(), $data = null, array $attributes = array())
+    /**
+     * @param string                                 $version
+     * @param array                                  $headers
+     * @param \Psr\Http\Message\StreamInterface|null $body
+     * @param string                                 $requestTarget
+     * @param string                                 $method
+     * @param \Psr\Http\Message\UriInterface|null    $uri
+     * @param array                                  $server
+     * @param array                                  $cookies
+     * @param array                                  $query
+     * @param array                                  $uploadedFiles
+     * @param array|null                             $data
+     * @param array                                  $attributes
+     */
+    public function __construct($version = '1.1', array $headers = [], StreamInterface $body = null, $requestTarget = '/', $method = 'GET', UriInterface $uri = null, array $server = [], array $cookies = [], array $query = [], array $uploadedFiles = [], $data = null, array $attributes = [])
     {
         parent::__construct($version, $headers, $body);
 
@@ -64,7 +115,7 @@ class ServerRequest extends Message implements ServerRequestInterface
     {
         $this->method = $method;
 
-        return $this;
+        return clone $this;
     }
 
     public function getUri()
@@ -76,75 +127,266 @@ class ServerRequest extends Message implements ServerRequestInterface
     {
         $this->uri = $uri;
 
-        return $this;
+        return clone $this;
     }
 
+    /**
+     * Retrieve server parameters.
+     *
+     * Retrieves data related to the incoming request environment,
+     * typically derived from PHP's $_SERVER superglobal. The data IS NOT
+     * REQUIRED to originate from $_SERVER.
+     *
+     * @return array
+     */
     public function getServerParams()
     {
         return $this->server;
     }
 
+    /**
+     * Retrieve cookies.
+     *
+     * Retrieves cookies sent by the client to the server.
+     *
+     * The data MUST be compatible with the structure of the $_COOKIE
+     * superglobal.
+     *
+     * @return array
+     */
     public function getCookieParams()
     {
         return $this->cookies;
     }
 
+    /**
+     * Return an instance with the specified cookies.
+     *
+     * The data IS NOT REQUIRED to come from the $_COOKIE superglobal, but MUST
+     * be compatible with the structure of $_COOKIE. Typically, this data will
+     * be injected at instantiation.
+     *
+     * This method MUST NOT update the related Cookie header of the request
+     * instance, nor related values in the server params.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * updated cookie values.
+     *
+     * @param array $cookies Array of key/value pairs representing cookies.
+     * @return static
+     */
     public function withCookieParams(array $cookies)
     {
         throw new \BadMethodCallException('Not implemented.');
     }
 
+    /**
+     * Retrieve query string arguments.
+     *
+     * Retrieves the deserialized query string arguments, if any.
+     *
+     * Note: the query params might not be in sync with the URI or server
+     * params. If you need to ensure you are only getting the original
+     * values, you may need to parse the query string from `getUri()->getQuery()`
+     * or from the `QUERY_STRING` server param.
+     *
+     * @return array
+     */
     public function getQueryParams()
     {
         return $this->query;
     }
 
+    /**
+     * Return an instance with the specified query string arguments.
+     *
+     * These values SHOULD remain immutable over the course of the incoming
+     * request. They MAY be injected during instantiation, such as from PHP's
+     * $_GET superglobal, or MAY be derived from some other value such as the
+     * URI. In cases where the arguments are parsed from the URI, the data
+     * MUST be compatible with what PHP's parse_str() would return for
+     * purposes of how duplicate query parameters are handled, and how nested
+     * sets are handled.
+     *
+     * Setting query string arguments MUST NOT change the URI stored by the
+     * request, nor the values in the server params.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * updated query string arguments.
+     *
+     * @param array $query Array of query string arguments, typically from
+     *     $_GET.
+     * @return static
+     */
     public function withQueryParams(array $query)
     {
         $this->query = $query;
 
-        return $this;
+        return clone $this;
     }
 
+    /**
+     * Retrieve normalized file upload data.
+     *
+     * This method returns upload metadata in a normalized tree, with each leaf
+     * an instance of Psr\Http\Message\UploadedFileInterface.
+     *
+     * These values MAY be prepared from $_FILES or the message body during
+     * instantiation, or MAY be injected via withUploadedFiles().
+     *
+     * @return array An array tree of UploadedFileInterface instances; an empty
+     *     array MUST be returned if no data is present.
+     */
     public function getUploadedFiles()
     {
         return $this->uploadedFiles;
     }
 
+    /**
+     * Create a new instance with the specified uploaded files.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * updated body parameters.
+     *
+     * @param array $uploadedFiles An array tree of UploadedFileInterface instances.
+     * @return static
+     * @throws \InvalidArgumentException if an invalid structure is provided.
+     */
     public function withUploadedFiles(array $uploadedFiles)
     {
         throw new \BadMethodCallException('Not implemented.');
     }
 
+    /**
+     * Retrieve any parameters provided in the request body.
+     *
+     * If the request Content-Type is either application/x-www-form-urlencoded
+     * or multipart/form-data, and the request method is POST, this method MUST
+     * return the contents of $_POST.
+     *
+     * Otherwise, this method may return any results of deserializing
+     * the request body content; as parsing returns structured content, the
+     * potential types MUST be arrays or objects only. A null value indicates
+     * the absence of body content.
+     *
+     * @return null|array|object The deserialized body parameters, if any.
+     *     These will typically be an array or object.
+     */
     public function getParsedBody()
     {
         return $this->data;
     }
 
+    /**
+     * Return an instance with the specified body parameters.
+     *
+     * These MAY be injected during instantiation.
+     *
+     * If the request Content-Type is either application/x-www-form-urlencoded
+     * or multipart/form-data, and the request method is POST, use this method
+     * ONLY to inject the contents of $_POST.
+     *
+     * The data IS NOT REQUIRED to come from $_POST, but MUST be the results of
+     * deserializing the request body content. Deserialization/parsing returns
+     * structured data, and, as such, this method ONLY accepts arrays or objects,
+     * or a null value if nothing was available to parse.
+     *
+     * As an example, if content negotiation determines that the request data
+     * is a JSON payload, this method could be used to create a request
+     * instance with the deserialized parameters.
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * updated body parameters.
+     *
+     * @param null|array|object $data The deserialized body data. This will
+     *     typically be in an array or object.
+     * @return static
+     * @throws \InvalidArgumentException if an unsupported argument type is
+     *     provided.
+     */
     public function withParsedBody($data)
     {
         $this->data = $data;
 
-        return $this;
+        return clone $this;
     }
 
+    /**
+     * Retrieve attributes derived from the request.
+     *
+     * The request "attributes" may be used to allow injection of any
+     * parameters derived from the request: e.g., the results of path
+     * match operations; the results of decrypting cookies; the results of
+     * deserializing non-form-encoded message bodies; etc. Attributes
+     * will be application and request specific, and CAN be mutable.
+     *
+     * @return array Attributes derived from the request.
+     */
     public function getAttributes()
     {
         return $this->attributes;
     }
 
+    /**
+     * Retrieve a single derived request attribute.
+     *
+     * Retrieves a single derived request attribute as described in
+     * getAttributes(). If the attribute has not been previously set, returns
+     * the default value as provided.
+     *
+     * This method obviates the need for a hasAttribute() method, as it allows
+     * specifying a default value to return if the attribute is not found.
+     *
+     * @see getAttributes()
+     * @param string $name The attribute name.
+     * @param mixed $default Default value to return if the attribute does not exist.
+     * @return mixed
+     */
     public function getAttribute($name, $default = null)
     {
         return isset($this->attributes[$name]) ? $this->attributes[$name] : $default;
     }
 
+    /**
+     * Return an instance with the specified derived request attribute.
+     *
+     * This method allows setting a single derived request attribute as
+     * described in getAttributes().
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that has the
+     * updated attribute.
+     *
+     * @see getAttributes()
+     * @param string $name The attribute name.
+     * @param mixed $value The value of the attribute.
+     * @return static
+     */
     public function withAttribute($name, $value)
     {
         $this->attributes[$name] = $value;
 
-        return $this;
+        return clone $this;
     }
 
+    /**
+     * Return an instance that removes the specified derived request attribute.
+     *
+     * This method allows removing a single derived request attribute as
+     * described in getAttributes().
+     *
+     * This method MUST be implemented in such a way as to retain the
+     * immutability of the message, and MUST return an instance that removes
+     * the attribute.
+     *
+     * @see getAttributes()
+     * @param string $name The attribute name.
+     * @return static
+     */
     public function withoutAttribute($name)
     {
         throw new \BadMethodCallException('Not implemented.');
