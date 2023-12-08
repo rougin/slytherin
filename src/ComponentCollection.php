3ae912ae7e84cadc277d00f4a39712211b52018d
Rougin Gutib <rougingutib@gmail.com>
2023-12-08 10:02:17 +0800

    Fix compatibility issues from v0.4.4

--- src/ComponentCollection.php
+++ src/ComponentCollection.php
@@ -6,8 +6,11 @@ namespace Rougin\Slytherin;
  * Component Collection
  *
  * Contains all the required components for Slytherin.
- * 
+ * NOTE: To be removed in v1.0.0. Use "Components" instead.
+ *
  * @package Slytherin
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
-class ComponentCollection extends Components {}
+class ComponentCollection extends Components
+{
+}
--- src/Components.php
+++ src/Components.php
@@ -46,6 +46,28 @@ class Components
     }
 
     /**
+     * Gets an instance of the dependency injector.
+     * NOTE: To be removed in v1.0.0. Use "getContainer" instead.
+     * 
+     * @return \Rougin\Slytherin\IoC\ContainerInterface
+     */
+    public function getDependencyInjector()
+    {
+        return $this->getContainer();
+    }
+
+    /**
+     * Sets the dependency injector.
+     * NOTE: To be removed in v1.0.0. Use "setContainer" instead.
+     * 
+     * @param \Rougin\Slytherin\IoC\ContainerInterface $injector
+     */
+    public function setDependencyInjector(ContainerInterface $injector)
+    {
+        return $this->setContainer($injector);
+    }
+
+    /**
      * Gets the dispatcher.
      * 
      * @return \Rougin\Slytherin\Dispatching\DispatcherInterface
@@ -83,6 +105,28 @@ class Components
     public function setDebugger(DebuggerInterface $debugger)
     {
         return $this->setComponent('debugger', $debugger);
+    }
+
+    /**
+     * Gets the debugger.
+     * NOTE: To be removed in v1.0.0. Use "getDebugger" instead.
+     * 
+     * @return \Rougin\Slytherin\Debug\DebuggerInterface
+     */
+    public function getErrorHandler()
+    {
+        return $this->getDebugger();
+    }
+
+    /**
+     * Sets the debugger.
+     * NOTE: To be removed in v1.0.0. Use "setDebugger" instead.
+     * 
+     * @param  \Rougin\Slytherin\Debug\DebuggerInterface $debugger
+     */
+    public function setErrorHandler(DebuggerInterface $debugger)
+    {
+        return $this->setDebugger($debugger);
     }
 
     /**
