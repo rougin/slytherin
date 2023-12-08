394c210391b2c732be5e8cb70085e9c7761a8bb4
Rougin Gutib <rougingutib@gmail.com>
2023-12-08 16:08:02 +0800

    Change details in TwigRenderer

--- src/Template/Twig.php
+++ src/Template/Twig.php
@@ -3,17 +3,13 @@
 namespace Rougin\Slytherin\Template;
 
 /**
- * Twig Renderer
+ * Renderer
  *
- * A simple implementation of a template renderer that is based on top of
- * Sensiolab's Twig - a flexible, fast, and secure template engine for PHP.
- * NOTE: To be removed in v1.0.0. Use "Template\TwigRenderer" instead.
- *
- * http://twig.sensiolabs.org
- *
+ * NOTE: To be removed in v1.0.0. Use "TwigRenderer" instead.
+ * 
  * @package Slytherin
  * @author  Rougin Gutib <rougingutib@gmail.com>
  */
-class Twig extends Twig\Renderer
+class Twig extends TwigRenderer
 {
 }
